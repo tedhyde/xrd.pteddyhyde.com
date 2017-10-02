2d8ee5fae8f90a2192a13a4bef6aee9574ae4cb3
Chris Dawson <xrdawson@gmail.com>
2017-10-01 21:32:08 -0700

    Simplify client

--- setup.js
+++ setup.js
@@ -1,27 +1,3 @@
-var iframe;
-
-function setup() {
-    iframe = document.getElementById( "th" ).contentWindow;
-    window.addEventListener( 'message', receiveMessage );
-}
-
-function receiveMessage( event ){
-    if (event.origin === "https://www.teddyhyde.com") {
-	setDisplayPs( "inline-block" );
-	document.getElementById("th").style.display = "none";
-    }
-    else {
-	console.log( "Origin", event.origin );
-    }
-}
-
-function setDisplayPs( value ) {
-    const ps = document.getElementsByTagName("p");
-    for( var i = 1; i < ps.length; i++ ) {
-	var p = ps[i];
-	p.style.display = value;
-    }
-}
 
 window.onload = function(e) {
     setup();
